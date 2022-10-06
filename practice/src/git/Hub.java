d696c06290000f6ce0c53ffe5b7e819ebb2f1df1
Govinda1414 <114895404+Govinda1414@users.noreply.github.com>
2022-10-06 18:22:04 +0530

    code change
    hello

--- practice/src/git/Hub.java
+++ practice/src/git/Hub.java
@@ -5,6 +5,7 @@ public class Hub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("1st class complete");
+System.out.println("hot nahi re");
 	}
 
 }
